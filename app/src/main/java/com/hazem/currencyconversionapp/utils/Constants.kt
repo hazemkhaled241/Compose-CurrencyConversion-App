6ef261a7f0dd923d71719d8664a360d755585b53
hazem khaled <hazemk778@gmail.com>
2023-08-23 14:49:40 +0300

    Create Constants.kt object to avoid hard coded

---
+++ app/src/main/java/com/hazem/currencyconversionapp/utils/Constants.kt
@@ -0,0 +1,6 @@
+package com.hazem.currencyconversionapp.utils
+
+object Constants {
+    const val CURRENCY_CONVERSION_API =
+        "https://currencyexchange-wbtr.onrender.com/pair/{base}/{target}"
+}
