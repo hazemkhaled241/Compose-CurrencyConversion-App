c62add95411f822181e955c93758703ccfc294e5
dell <zynab5783@gmail.com>
2023-08-28 02:16:58 +0300

    main ui

--- app/src/main/java/com/hazem/currencyconversionapp/presentation/MainScreen.kt
+++ app/src/main/java/com/hazem/currencyconversionapp/presentation/MainScreen.kt
@@ -1,7 +1,9 @@
 package com.hazem.currencyconversionapp.presentation
 
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -10,13 +12,16 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.navigation.NavController
 import com.hazem.currencyconversionapp.presentation.currency_comparison.Comparison
 import com.hazem.currencyconversionapp.presentation.currency_conversion.components.Conversion
 import com.hazem.currencyconversionapp.presentation.main_component.ConvertAndCompareToggle
+import com.hazem.currencyconversionapp.presentation.main_component.Portfolio
 import com.hazem.currencyconversionapp.presentation.main_component.TopOfScreen
 
 @Composable
 fun MainScreen(
+    navController: NavController
 ) {
 
     var isConvertClicked by remember {
@@ -25,61 +30,65 @@ fun MainScreen(
     var isCompareClicked by remember {
         mutableStateOf(false)
     }
-    ConstraintLayout {
-        val (topScreen, toggleButton, converterCard) = createRefs()
-        Box(
-            modifier = Modifier
-                .constrainAs(topScreen) {
+    Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+        ConstraintLayout {
+            val (topScreen, toggleButton, converterCard, portfolio) = createRefs()
+            Box(
+                modifier = Modifier
+                    .constrainAs(topScreen) {
+                        top.linkTo(parent.top)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                    }
+            ) {
+                TopOfScreen()
+            }
+            Box(
+                modifier = Modifier
+                    .constrainAs(toggleButton) {
+                        top.linkTo(parent.top, margin = 280.dp)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                    }
+            ) {
+                ConvertAndCompareToggle(onCompareClicked = {
+                    isConvertClicked = false
+                    isCompareClicked = true
 
-                    top.linkTo(parent.top)
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                }
-        ) {
-            TopOfScreen()
-
-        }
-
-        Box(
-            modifier = Modifier
-                .constrainAs(toggleButton) {
-                    top.linkTo(parent.top, margin = 280.dp)
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                }
-        ) {
-            ConvertAndCompareToggle(onCompareClicked = {
-                isConvertClicked = false
-                isCompareClicked = true
-            },
-
-                onConvertClicked = {
+                }, onConvertClicked = {
                     isConvertClicked = true
                     isCompareClicked = false
                 })
-        }
+            }
 
-        Box(
-            modifier = Modifier
+            Box(modifier = Modifier
                 .constrainAs(converterCard) {
                     top.linkTo(topScreen.bottom)
                     start.linkTo(parent.start)
                     end.linkTo(parent.end)
+                }) {
+                androidx.compose.animation.AnimatedVisibility(
+                    visible = isConvertClicked
+                ) {
+                    Conversion()
                 }
-        ) {
-            AnimatedVisibility(
-                visible = isConvertClicked
-            ) {
-                Conversion()
+
+                androidx.compose.animation.AnimatedVisibility(
+                    visible = isCompareClicked
+                ) {
+                    Comparison()
+                }
+            }
+            Box(modifier = Modifier
+                .constrainAs(portfolio) {
+                    top.linkTo(converterCard.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }) {
+                Portfolio(navController = navController)
             }
 
-            AnimatedVisibility(
-                visible = isCompareClicked
-            ) {
-                Comparison()
 
-            }
         }
     }
 }
-
