95c7af42b723cc2cb74ac9508bd58163c1836bd5
dell <zynab5783@gmail.com>
2023-08-28 20:06:53 +0300

    rename SerializedName to camelCase

--- app/src/main/java/com/hazem/currencyconversionapp/data/remote/dto/ConversionResponseDto.kt
+++ app/src/main/java/com/hazem/currencyconversionapp/data/remote/dto/ConversionResponseDto.kt
@@ -3,6 +3,7 @@ package com.hazem.currencyconversionapp.data.remote.dto
 import com.google.gson.annotations.SerializedName
 
 data class ConversionResponseDto(
+
     @SerializedName("conversionRate")
     val conversionRate: String,
     @SerializedName("conversionValue")
