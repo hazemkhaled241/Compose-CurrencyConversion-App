19f3ba537813d3684b8b25629581025ddb361abe
hazem khaled <hazemk778@gmail.com>
2023-08-29 00:56:59 +0300

    test data layer successfully

--- app/src/test/java/com/hazem/currencyconversionapp/data/repository/local/FakeCurrencies.kt
+++
@@ -1,8 +0,0 @@
-package com.hazem.currencyconversionapp.data.repository.local
-
-import com.hazem.currencyconversionapp.data.local.entities.CurrencyEntity
-
-val currenciesList = listOf(
-    CurrencyEntity(1,"USD"),
-    CurrencyEntity(2,"EGP")
-)
