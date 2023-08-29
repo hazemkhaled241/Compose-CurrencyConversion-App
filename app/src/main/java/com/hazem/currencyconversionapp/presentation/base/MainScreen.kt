bd58d58e4ccb02fe96d3429f34747c606593ecb3
dell <zynab5783@gmail.com>
2023-08-29 16:24:44 +0300

    put main screen in new package

--- app/src/main/java/com/hazem/currencyconversionapp/presentation/base/MainScreen.kt
+++ app/src/main/java/com/hazem/currencyconversionapp/presentation/base/MainScreen.kt
@@ -1,5 +1,4 @@
-
-package com.hazem.currencyconversionapp.presentation
+package com.hazem.currencyconversionapp.presentation.base
 
 import android.util.Log
 import androidx.compose.foundation.layout.Box
@@ -10,10 +9,6 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -32,19 +27,10 @@ import com.hazem.currencyconversionapp.presentation.main_component.TopOfScreen
 @Composable
 fun MainScreen(
     currencyConversionViewModel: CurrencyConversionViewModel = hiltViewModel(),
+    mainViewModel: MainViewModel = hiltViewModel(),
     navController: NavController
 ) {
 
-    var isConvertClicked by remember {
-        mutableStateOf(true)
-    }
-    var isCompareClicked by remember {
-        mutableStateOf(false)
-    }
-    var pageName by remember {
-        mutableStateOf("Currency Converter")
-    }
-   
     currencyConversionViewModel.onEvent(CurrencyEvents.GetAllFavoriteCurrencies)
 //if(currencyConversionViewModel.getAllFavoriteCurrencyState.value.isLoading){
 //    Box(modifier = Modifier.fillMaxSize()){
@@ -53,30 +39,20 @@ fun MainScreen(
 //    }
 //}
     val favList = currencyConversionViewModel.getAllFavoriteCurrencyState.value.allCurrency
-
     val favoriteList = favList.map { it.currency }
     if (!currencyConversionViewModel.getAllFavoriteCurrencyState.value.isLoading) {
-        LaunchedEffect(key1 =currencyConversionViewModel.currencyBase) {
+        LaunchedEffect(key1 = currencyConversionViewModel.currencyBase) {
             Log.d("kkk", favoriteList.toString())
-         if (currencyConversionViewModel.currencyBase.isNotEmpty()){
+
             currencyConversionViewModel.onEvent(
                 CurrencyEvents.GetFavoritesDetails(
                     base = currencyConversionViewModel.currencyBase,
                     currencies = favoriteList
                 )
             )
-         }else{
-             currencyConversionViewModel.onEvent(
-                 CurrencyEvents.GetFavoritesDetails(
-                     base = "USD",
-                     currencies = favoriteList
-                 )
-             )
-         }
-
         }
     }
-    Box(modifier = Modifier.fillMaxSize()){
+    Box(modifier = Modifier.fillMaxSize()) {
 
         val liveExchangeList = currencyConversionViewModel.currencyDetailsState.value.favorites
 
@@ -92,7 +68,7 @@ fun MainScreen(
                         }
                 ) {
 
-                    TopOfScreen(pageName = pageName)
+                    TopOfScreen(pageName = mainViewModel.pageNameState)
                 }
                 Box(
                     modifier = Modifier
@@ -102,18 +78,7 @@ fun MainScreen(
                             end.linkTo(parent.end)
                         }
                 ) {
-                    ConvertAndCompareToggle(onCompareClicked = {
-                        isConvertClicked = false
-                        isCompareClicked = true
-                        pageName = "Currency Compare"
-
-                    },
-                        onConvertClicked = {
-                        isConvertClicked = true
-                        isCompareClicked = false
-                        pageName = "Currency converter"
-
-                    })
+                    ConvertAndCompareToggle()
                 }
 
                 Box(modifier = Modifier
@@ -122,15 +87,17 @@ fun MainScreen(
                         start.linkTo(parent.start)
                         end.linkTo(parent.end)
                     }) {
+
                     androidx.compose.animation.AnimatedVisibility(
-                        visible = isConvertClicked
+                        visible = mainViewModel.isConvertClickedState.value
                     ) {
                         Conversion()
                     }
 
                     androidx.compose.animation.AnimatedVisibility(
-                        visible = isCompareClicked
+                        visible = mainViewModel.isCompareClickedState.value
                     ) {
+
                         Comparison()
                     }
                 }
@@ -142,14 +109,18 @@ fun MainScreen(
                         bottom.linkTo(parent.bottom)
                     }) {
                     if (!currencyConversionViewModel.currencyDetailsState.value.isLoading) {
-                        Portfolio(navController = navController, list = liveExchangeList)}
+                        Portfolio(navController = navController, list = liveExchangeList)
+                    }
                 }
 
 
             }
         }
         if (currencyConversionViewModel.currencyDetailsState.value.isLoading) {
-            CircularProgressIndicator(modifier = Modifier.align(Alignment.Center), color = Color.Blue)
+            CircularProgressIndicator(
+                modifier = Modifier.align(Alignment.Center),
+                color = Color.Blue
+            )
         }
     }
 }
--- app/src/main/java/com/hazem/currencyconversionapp/presentation/ui/Navigation.kt
+++ app/src/main/java/com/hazem/currencyconversionapp/presentation/ui/Navigation.kt
@@ -4,7 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.hazem.currencyconversionapp.presentation.MainScreen
+import com.hazem.currencyconversionapp.presentation.base.MainScreen
 import com.hazem.currencyconversionapp.presentation.currency_conversion.components.Favorite
 
 @Composable
